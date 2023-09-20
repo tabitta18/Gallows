3dbb13f90f675637679df95d4ebd3532f2b4e322
Tabitha Cat <134290478+tabitta18@users.noreply.github.com>
2023-09-20 17:37:30 +0500

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -5,7 +5,7 @@ eel.init("web")
 
 # Слова для игры (можно добавить больше слов)
 words = ["apple", "banana", "cherry", "date", "elderberry","word","hellow",
-        "house", "history"
+        "house", "history", "humen"
         ]
 
 @eel.expose
